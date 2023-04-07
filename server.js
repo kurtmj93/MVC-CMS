82446607c0b2bb258b44b814ee120733fca75593
Kurtmj93 <kurtmj93@gmail.com>
2023-04-07 14:08:24 -0400

    write sequelize connection, schema.sql, server.js to connect to db

---
+++ server.js
@@ -0,0 +1,47 @@
+// require express & call as function
+const express = require('express');
+const app = express();
+// call for express session package to handle user sessions and handlebars for app engine
+const session = require('express-session');
+const handlebars = require('express-handlebars');
+
+// connect to db
+const sequelize = require('./config/connection');
+// connect express session to db
+const SequelizeStore = require('connect-session-sequelize')(
+  session.Store
+);
+
+// set and use session connection info
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  app.use(session(sess));
+
+// set handlebars as view engine
+const bars = handlebars.create({ helpers });
+app.engine('handlebars', bars.engine);
+app.set('view engine', 'handlebars');
+
+// express middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set .env PORT, or localhost:3001
+const PORT = process.env.PORT || 3001;
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => console.log('Now listening'));
+  });
